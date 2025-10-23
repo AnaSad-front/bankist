817e09bf94fd7b3423372d5dc9fdddf6b2030932
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-10-23 17:50:56 +0300

    coding challenge 1

---
+++ challenges.js
@@ -0,0 +1,26 @@
+// CODING CHALLENGE 1
+/*
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+
+const dogsJulia2 = [9, 16, 6, 8, 3];
+const dogsKate2 = [10, 5, 6, 1, 4];
+
+const checkDogs = function (arr1, arr2) {
+  const arr1Copy = arr1.slice(1, -2);
+  console.log(arr1Copy);
+
+  const arrCorrect = arr1Copy.concat(arr2);
+  console.log(arrCorrect);
+
+  arrCorrect.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+    }
+  });
+};
+checkDogs(dogsJulia, dogsKate);
+checkDogs(dogsJulia2, dogsKate2);
+*/
