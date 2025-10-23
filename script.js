83d576ae1586b5ad728563f7f7e29995021bb51e
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-10-23 15:57:07 +0300

    simple array methods

--- script.js
+++ script.js
@@ -74,3 +74,38 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// SIMPLE ARRAY METHODS
+/*
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// slice method
+console.log(arr.slice(2)); // creates a new array starting from index 2 to the end
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2)); // last two elements
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2)); // from index 1 to the second last element
+console.log(arr.slice()); // shallow copy of the array
+console.log([...arr]); // also creates a shallow copy using spread operator
+
+// splice method
+// console.log(arr.splice(2)); // mutates the original array, removes elements from index 2 to the end
+arr.splice(-1); // removes the last element
+console.log(arr);
+arr.splice(1, 2); // removes 2 elements starting from index 1
+console.log(arr);
+
+// reverse method
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse()); // mutates the original array
+console.log(arr2);
+
+// concat method
+const letters = arr.concat(arr2); // does not mutate original arrays
+console.log(letters);
+console.log([...arr, ...arr2]); // using spread operator to concatenate
+
+// join method
+console.log(letters.join(' - ')); // joins elements into a string with ' - ' as separator
+*/
