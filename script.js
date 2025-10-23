294c3918bacb2552b0db34b459b359bdefe3b9fd
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-10-23 16:40:33 +0300

    forEach method

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -124,4 +124,26 @@ console.log(arr.at(-1));
 // also works on strings
 console.log('anastasia'.at(0)); 
 console.log('anastasia'.at(-1));
+*/
+
+// LOOPING ARRAYS: forEach
+/*
+const movements = [200, -100, 340, -300, -20, 50, 400, -460];
+
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('-----FOREACH-----');
+movements.forEach(function (movement, i, arr) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
 */
